0a16e1ec266fcd5223e59a1d4983e65c2b2cf1e4
alex <apocthiel@gmail.com>
2018-08-04 16:52:43 -0700

    Updated volume controls, added compton to dotfiles

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,9 +3,16 @@
 sudo apt install --assume-yes git
 
 packagelist=(
+
+    # Applications
     arandr                  # Monitor Settings
+    qdirstat                # For exploring filesystems easily
+    pavucontrol             # Easily work with sound devices
+
+    # awesome specific things
     pasystray               # Sound Widget
-    network-manager-gnome   # Bluetooth
+    network-manager-gnome   # For wifi and network info
+    blueman                 # For bluetooth stuff
     compton                 # For drop-shadows
     xautolock               # For locking my window within a timespan
     i3lock                  # For the lockscreen
--- move_dotfiles.py
+++ move_dotfiles.py
@@ -4,26 +4,12 @@ import shutil
 import os
 
 
-def copy_dir(source, dest, overwrite_existing_files=False):
-    dest.mkdir(parents=True, exist_ok=True)
-    print("Creating", source, dest)
-
-    for path in Path(source).iterdir():
-        # Make sure all directories exist
-        if path.is_dir():
-            copy_dir(path, dest / path.name, overwrite_existing_files)
-
-        if path.is_file():
-            if (dest / path.name).exists():
-                if overwrite_existing_files:
-                    os.remove(dest / path.name)
-                    shutil.copy(str(path), str(dest / path.name))
-                    print("Overwriting", str(dest / path.name))
-                else:
-                    print(str(path), "already exists! Skipping copy...")
-            else:
-                shutil.copy(str(path),
-                            str(dest / path.name))
+def try_symlink(source, dest):
+    try:
+        os.symlink(source, dest)
+        print("Symlinked", source, "to", dest)
+    except FileExistsError:
+        print("Skipping symlink-", dest, "already exists!")
 
 
 def main():
@@ -31,9 +17,13 @@ def main():
     dotfiles = Path(".").resolve()
     config = Path(".config")
     awesome = config / "awesome/"
+    compton = config / "compton.conf"
 
     # Copy awesome configs
-    os.symlink(dotfiles / awesome, home / awesome)
+    try_symlink(dotfiles / awesome, home / awesome)
+
+    # Copy compton config
+    try_symlink(dotfiles / compton, home / compton)
 
 
 if __name__ == "__main__":
