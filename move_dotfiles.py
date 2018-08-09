8e10845b7a43d809e31ef8e4b45bac3ec00171f7
alex <apocthiel@gmail.com>
2018-08-08 23:14:16 -0700

    Added a patch for chrome-remote-desktop.patch and an automated python script to add it

--- move_dotfiles.py
+++ move_dotfiles.py
@@ -1,7 +1,8 @@
-from argparse import ArgumentParser
-from pathlib import Path
-import shutil
 import os
+import shutil
+import subprocess
+from pathlib import Path
+from argparse import ArgumentParser
 
 
 def try_symlink(source, dest):
@@ -12,18 +13,35 @@ def try_symlink(source, dest):
         print("Skipping symlink-", dest, "already exists!")
 
 
+def try_patching(existing_file, patchfile):
+    if not existing_file.exists():
+        print("ERROR: Cannot find", existing_file, ", unable to patch!")
+        return
+    assert patchfile.exists()
+    print("AYY", "sudo patch " + str(existing_file) + " " + str(patchfile))
+    subprocess.check_call(["sudo", "patch",
+                           str(existing_file), str(patchfile)])
+
+
 def main():
     home = Path("/home/alex/").resolve()
     dotfiles = Path(".").resolve()
     config = Path(".config")
     awesome = config / "awesome/"
     compton = config / "compton.conf"
+    remote_desktop = Path("/opt/google/chrome-remote-desktop") / \
+                     "chrome-remote-desktop"
+    remote_desktop_patch = Path(
+        "./patches/chrome-remote-desktop.patch").resolve()
 
     # Copy awesome configs
     try_symlink(dotfiles / awesome, home / awesome)
 
     # Copy compton config
     try_symlink(dotfiles / compton, home / compton)
+
+    # Try patching chrome remote desktop file
+    try_patching(remote_desktop, remote_desktop_patch)
 
 
 if __name__ == "__main__":
