a248c741332a251e33e45ec88825de1a63214327
alex <alex.thiel@dilililabs.com>
2018-08-16 09:49:44 -0700

    Added a default open directory and made it work

--- default_env_vars.sh
+++ default_env_vars.sh
@@ -4,4 +4,4 @@
 export CHROME_REMOTE_DESKTOP_DEFAULT_DESKTOP_SIZES="1920x1080,1920x1080,1920x1080"
 
 # Default directory for file manager to open to
-export DEFAULT_OPEN_DIRECTORY="~/"
+export DEFAULT_OPEN_DIRECTORY=$HOME
