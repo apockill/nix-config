ecbdef8ec35283998225bd9d2875a57d288f0ed6
alex <alex_dilili@bitbucket.org>
2018-10-16 09:57:32 -0700

    Added themes for awesome

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -59,7 +59,8 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+beautiful.init("~/.config/awesome/paperlike/theme.lua")
+
 
 -- This is used later as the default terminal and editor to run.
 terminal = "dbus-launch gnome-terminal"
