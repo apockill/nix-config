76141fe808225edc190ecd0a66a9317f77102b58
alex <alex.thiel@dilililabs.com>
2019-03-28 16:00:56 -0700

    Now you can switch between all monitors with ctrl+modkey+#

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -433,6 +433,18 @@ clientkeys = gears.table.join(
 -- This should map on the top row of your keyboard, usually 1 to 9.
 for i = 1, 9 do
     globalkeys = gears.table.join(globalkeys,
+	-- Switch all monitors to the specified tag
+	awful.key({ modkey, "Control" }, "#" .. i + 9,
+                  function ()
+                      for screen = 1, screen.count() do
+                          local tag = awful.tag.gettags(screen)[i]
+                          if tag then
+                              awful.tag.viewonly(tag)
+                          end
+                      end
+                  end,
+                  {description = "switch all monitors to tag #"..i, group = "tag"}),
+
         -- View tag only.
         awful.key({ modkey }, "#" .. i + 9,
                   function ()
@@ -443,16 +455,7 @@ for i = 1, 9 do
                         end
                   end,
                   {description = "view tag #"..i, group = "tag"}),
-        -- Toggle tag display.
-        awful.key({ modkey, "Control" }, "#" .. i + 9,
-                  function ()
-                      local screen = awful.screen.focused()
-                      local tag = screen.tags[i]
-                      if tag then
-                         awful.tag.viewtoggle(tag)
-                      end
-                  end,
-                  {description = "toggle tag #" .. i, group = "tag"}),
+
         -- Move client to tag.
         awful.key({ modkey, "Shift" }, "#" .. i + 9,
                   function ()
@@ -463,18 +466,7 @@ for i = 1, 9 do
                           end
                      end
                   end,
-                  {description = "move focused client to tag #"..i, group = "tag"}),
-        -- Toggle tag on focused client.
-        awful.key({ modkey, "Control", "Shift" }, "#" .. i + 9,
-                  function ()
-                      if client.focus then
-                          local tag = client.focus.screen.tags[i]
-                          if tag then
-                              client.focus:toggle_tag(tag)
-                          end
-                      end
-                  end,
-                  {description = "toggle focused client on tag #" .. i, group = "tag"})
+                  {description = "move focused client to tag #"..i, group = "tag"})
     )
 end
 
