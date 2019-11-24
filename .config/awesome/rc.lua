f13d79ff0ccd14bf4e304138e09c961c8090536d
Alex Thiel <apocthiel@gmail.com>
2019-11-24 03:14:56 -0800

    Removed the 'menu' code for awesome-wm. Added a battery icon.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -124,23 +124,6 @@ myawesomemenu = {
 local menu_awesome = { "awesome", myawesomemenu, beautiful.awesome_icon }
 local menu_terminal = { "open terminal", terminal }
 
---[[
-if has_fdo then
-    mymainmenu = freedesktop.menu.build({
-        before = { menu_awesome },
-        after =  { menu_terminal }
-    })
-else
-    mymainmenu = awful.menu({
-        items = {
-                  menu_awesome,
-                  { "Debian", debian.menu.Debian_menu.Debian },
-                  menu_terminal,
-                }
-    })
-end
-]]
-
 mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
                                      menu = mymainmenu })
 
@@ -620,3 +603,6 @@ run_once('compton --config ~/.config/compton.conf -b')  -- the -b makes it run i
 
 -- Run the clipboard manager
 run_once('parcellite')  -- the -b makes it run in the background
+
+-- Run the battery icon
+run_once('cbatticon')
