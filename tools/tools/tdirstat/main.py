f3d7dc45ea27782ede3e85106daa4d3b9a2a6da5
Alex Thiel <apocthiel@gmail.com>
2020-01-25 03:53:50 -0800

    My directory crawler is now on-par with QDirstat in speed and counting abilities. It seems to be 'api compatible' with QDirstat in counting respects (count symlinks, don't follow mounts, etc).
    Furthermore, counting happens in a parallel thread, jobs are distributed evenly (tree is expanded evenly), and it is possible to know when crawling has finished at any given node.

--- tools/tools/tdirstat/main.py
+++ tools/tools/tdirstat/main.py
@@ -1,21 +1,23 @@
 from asciimatics.event import KeyboardEvent
-from asciimatics.widgets import Frame, Layout, FileBrowser, Widget, Label, PopUpDialog, Text, \
-    Divider
+from asciimatics.widgets import (
+    Frame, Layout,
+    MultiColumnListBox,
+    Widget,
+    Label,
+    PopUpDialog,
+    Text,
+    Divider)
 from asciimatics.scene import Scene
 from asciimatics.screen import Screen
 from asciimatics.exceptions import ResizeScreenError, StopApplication
 import sys
-import os
-try:
-    import magic
-except ImportError:
-    pass
 
 
-class DemoFrame(Frame):
+class TDirStatView(Frame):
     def __init__(self, screen):
-        super(DemoFrame, self).__init__(
-            screen, screen.height, screen.width, has_border=False, name="My Form")
+        super(TDirStatView, self).__init__(
+            screen, screen.height, screen.width, has_border=False,
+            name="My Form")
 
         # Create the (very simple) form layout...
         layout = Layout([1], fill_frame=True)
@@ -25,11 +27,16 @@ class DemoFrame(Frame):
         self._details = Text()
         self._details.disabled = True
         self._details.custom_colour = "field"
-        self._list = FileBrowser(Widget.FILL_FRAME,
-                                 os.path.abspath("."),
-                                 name="mc_list",
-                                 on_select=self.popup,
-                                 on_change=self.details)
+        titles = ["Name", "%", "Size", "Items"]
+        columns = [0] + [f"{len(title) + 5}>" for title in titles[1:]]
+        self._list = MultiColumnListBox(
+            height=Widget.FILL_FRAME,
+            columns=columns,
+            titles=titles,
+            options=[],
+            name="qdirstat_list",
+            on_select=self.popup,
+            on_change=self.details)
         layout.add_widget(Label("Local disk browser sample"))
         layout.add_widget(Divider())
         layout.add_widget(self._list)
@@ -43,20 +50,12 @@ class DemoFrame(Frame):
     def popup(self):
         # Just confirm whenever the user actually selects something.
         self._scene.add_effect(
-            PopUpDialog(self._screen, "You selected: {}".format(self._list.value), ["OK"]))
+            PopUpDialog(self._screen,
+                        "You selected: {}".format(self._list.value), ["OK"]))
 
     def details(self):
-        # If python magic is installed, provide a little more detail of the current file.
-        if self._list.value:
-            if os.path.isdir(self._list.value):
-                self._details.value = "Directory"
-            elif os.path.isfile(self._list.value):
-                try:
-                    self._details.value = magic.from_file(self._list.value)
-                except NameError:
-                    self._details.value = "File (run 'pip install python-magic' for more details)"
-        else:
-            self._details.value = "--"
+        print("Changing _details value")
+        self._details.value = "Haha I have been changed indeed"
 
     def process_event(self, event):
         # Do the key handling for this Frame.
@@ -65,12 +64,22 @@ class DemoFrame(Frame):
                 raise StopApplication("User quit")
 
         # Now pass on to lower levels for normal handling of the event.
-        return super(DemoFrame, self).process_event(event)
+        return super(TDirStatView, self).process_event(event)
 
 
 def demo(screen, old_scene):
-    screen.play([Scene([DemoFrame(screen)], -1)], stop_on_resize=True, start_scene=old_scene)
+    screen.play([Scene([TDirStatView(screen)], -1)], stop_on_resize=True,
+                start_scene=old_scene)
 
+
+"""Notes
+os.stat will give
+    - Size
+    
+def root_path():
+    '''Get the root directory (os independent)'''
+    return os.path.abspath(os.sep)
+"""
 
 last_scene = None
 while True:
