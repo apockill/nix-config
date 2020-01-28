a379dea9f1af88b95f49468c862a5f189e558b3a
alex <apocthiel@gmail.com>
2020-01-27 23:05:35 -0800

    Current progress: UI can technically update and show changing items, but only during 'process_event'

--- tools/tools/tdirstat/main.py
+++ tools/tools/tdirstat/main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+from argparse import ArgumentParser
+
 from asciimatics.event import KeyboardEvent
 from asciimatics.widgets import (
     Frame, Layout,
@@ -10,14 +14,17 @@ from asciimatics.widgets import (
 from asciimatics.scene import Scene
 from asciimatics.screen import Screen
 from asciimatics.exceptions import ResizeScreenError, StopApplication
-import sys
+
+from crawler import DirectoryStat
 
 
 class TDirStatView(Frame):
-    def __init__(self, screen):
+    def __init__(self, screen, dirstat: DirectoryStat):
         super(TDirStatView, self).__init__(
-            screen, screen.height, screen.width, has_border=False,
+            screen, screen.height, screen.width, has_border=True,
             name="My Form")
+        # Model
+        self.dirstat = dirstat
 
         # Create the (very simple) form layout...
         layout = Layout([1], fill_frame=True)
@@ -28,7 +35,7 @@ class TDirStatView(Frame):
         self._details.disabled = True
         self._details.custom_colour = "field"
         titles = ["Name", "%", "Size", "Items"]
-        columns = [0] + [f"{len(title) + 5}>" for title in titles[1:]]
+        columns = [0] + [f"{len(title) + 15}>" for title in titles[1:]]
         self._list = MultiColumnListBox(
             height=Widget.FILL_FRAME,
             columns=columns,
@@ -37,7 +44,7 @@ class TDirStatView(Frame):
             name="qdirstat_list",
             on_select=self.popup,
             on_change=self.details)
-        layout.add_widget(Label("Local disk browser sample"))
+        layout.add_widget(Label("TDirStat"))
         layout.add_widget(Divider())
         layout.add_widget(self._list)
         layout.add_widget(Divider())
@@ -47,6 +54,20 @@ class TDirStatView(Frame):
         # Prepare the Frame for use.
         self.fix()
 
+    def update(self, frame_no):
+        options = []
+        for dirstat in self.dirstat.children:
+            columns = [
+                dirstat.path,
+                "Not Implemented",
+                "Not Implemented",
+                str(dirstat.n_items)
+            ]
+            options.append((columns, "Delet this? (Not Implemented)"))
+        self._list.options = options
+
+        super().update(frame_no)
+
     def popup(self):
         # Just confirm whenever the user actually selects something.
         self._scene.add_effect(
@@ -54,7 +75,6 @@ class TDirStatView(Frame):
                         "You selected: {}".format(self._list.value), ["OK"]))
 
     def details(self):
-        print("Changing _details value")
         self._details.value = "Haha I have been changed indeed"
 
     def process_event(self, event):
@@ -64,27 +84,37 @@ class TDirStatView(Frame):
                 raise StopApplication("User quit")
 
         # Now pass on to lower levels for normal handling of the event.
-        return super(TDirStatView, self).process_event(event)
+        return super().process_event(event)
 
 
-def demo(screen, old_scene):
-    screen.play([Scene([TDirStatView(screen)], -1)], stop_on_resize=True,
-                start_scene=old_scene)
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("root_dir", default=os.getcwd(), nargs="?",
+                        help="The directory to analyze")
+    args = parser.parse_args()
+
+    dirstat = None
+
+    def demo(screen, old_scene):
+        nonlocal dirstat
+        if dirstat is None:
+            dirstat = DirectoryStat(
+                path=args.root_dir,
+                on_stats_change=lambda *args, **kwargs: screen.)
+        screen.play(
+            [Scene([TDirStatView(screen, dirstat)], duration=-1)],
+
+            stop_on_resize=True,
+            start_scene=old_scene)
+
+    last_scene = None
+    while True:
+        try:
+            Screen.wrapper(demo, catch_interrupt=True, arguments=[last_scene])
+            sys.exit(0)
+        except ResizeScreenError as e:
+            last_scene = e.scene
 
 
-"""Notes
-os.stat will give
-    - Size
-    
-def root_path():
-    '''Get the root directory (os independent)'''
-    return os.path.abspath(os.sep)
-"""
-
-last_scene = None
-while True:
-    try:
-        Screen.wrapper(demo, catch_interrupt=False, arguments=[last_scene])
-        sys.exit(0)
-    except ResizeScreenError as e:
-        last_scene = e.scene
+if __name__ == "__main__":
+    main()
