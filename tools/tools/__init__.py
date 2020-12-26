3e9cd044e53f6ed8300916de231da31c8f4521b3
Alex Thiel <apocthiel@gmail.com>
2020-12-26 10:44:31 -0800

    Add recursive git garbage collection command

--- tools/tools/__init__.py
+++ tools/tools/__init__.py
@@ -1,2 +1,3 @@
 from .unix_scripts import ls
 from .file_explorer import file_explorer
+from .git_gc import git_gc
---
+++ tools/tools/git_gc.py
@@ -0,0 +1,26 @@
+from argparse import ArgumentParser
+from subprocess import check_call
+from pathlib import Path
+
+
+def iter_git_repos(root_path: Path):
+    for path in Path(root_path).iterdir():
+        if path.is_dir():
+            if (path / ".git").is_dir():
+                yield path
+                continue
+
+            for child_path in iter_git_repos(path):
+                yield child_path
+
+
+def git_gc():
+    parser = ArgumentParser(
+        description="Recursively finds git repos and runs 'git gc'")
+    parser.add_argument("root_dir", type=Path, nargs="?", default=Path("."))
+    args = parser.parse_args()
+
+    for git_dir in iter_git_repos(args.root_dir):
+        print(f"Clearing {git_dir}")
+        check_call(["git", "gc"],
+                   cwd=str(git_dir.resolve()))
