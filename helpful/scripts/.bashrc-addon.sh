9f5450c03580f85582a05b47a47ac18e49180fe8
Alex Thiel <apocthiel@gmail.com>
2021-05-14 14:06:26 -0700

    Forgot to add script

---
+++ helpful/scripts/.bashrc-addon.sh
@@ -0,0 +1,7 @@
+# Alex Helpful Aliases
+alias docker-killall='docker container stop $(docker container ls -aq) && docker container rm $(docker container ls -aq)'
+alias bf='brainframe'
+alias bfc='brainframe compose'
+export HDDL_INSTALL_DIR=/opt/intel/openvino/deployment_tools/inference_engine/external/hddl/
+
+
