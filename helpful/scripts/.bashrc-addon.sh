f017d3d5213159c2bc9ab0cf385eea7d04e6b033
Alex Thiel <apocthiel@gmail.com>
2021-05-19 17:45:02 -0700

    Added yet another cool alias

--- helpful/scripts/.bashrc-addon.sh
+++ helpful/scripts/.bashrc-addon.sh
@@ -2,6 +2,7 @@
 alias docker-killall='docker container stop $(docker container ls -aq) && docker container rm $(docker container ls -aq)'
 alias bf='brainframe'
 alias bfc='brainframe compose'
+alias grepnt="grep --invert-match"
 export HDDL_INSTALL_DIR=/opt/intel/openvino/deployment_tools/inference_engine/external/hddl/
 
 
