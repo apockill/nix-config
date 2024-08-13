6ca2c4ac8681d3e6471366cfa56b00d107a95e3d
Alex Thiele <apocthiel@gmail.com>
2024-08-13 10:42:37 -0700

    Update disable-hotbar-super-keys.sh

--- helpful/scripts/disable-hotbar-super-keys.sh
+++ helpful/scripts/disable-hotbar-super-keys.sh
@@ -8,8 +8,11 @@ gsettings set org.gnome.shell.extensions.dash-to-dock hot-keys false
 
 
 set -e
-for NUMBER in 1 2 3 4
-do 
-	gsettings set org.gnome.shell.extensions.dash-to-dock app-hotkey-$NUMBER '[]'
-	gsettings set org.gnome.shell.keybindings switch-to-application-$NUMBER '[]'
+gsettings set org.gnome.mutter dynamic-workspaces false
+gsettings set org.gnome.desktop.wm.preferences num-workspaces 9
+for i in {1..4}; do 
+  gsettings set "org.gnome.shell.keybindings" "switch-to-application-${i}" "[]"
+  gsettings set "org.gnome.desktop.wm.keybindings" "switch-to-workspace-${i}" "['<Super>${i}']"
+  gsettings set "org.gnome.desktop.wm.keybindings" "move-to-workspace-${i}" "['<Super><Alt>${i}']"
+  gsettings set "org.gnome.shell.extensions.dash-to-dock" "app-hotkey-${i}" "[]"
 done
