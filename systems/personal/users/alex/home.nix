7d330032ec6817a573ecf0c20975955825429322
Alex Thiele <apocthiel@gmail.com>
2025-01-12 15:49:25 -0800

    Add the rest of the custom shortcuts for applications

--- systems/personal/users/alex/home.nix
+++ systems/personal/users/alex/home.nix
@@ -4,13 +4,38 @@
     "org/gnome/settings-daemon/plugins/media-keys" = {
       custom-keybindings = [
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/screenshot-key/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/file-explorer-key/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/system-monitor-key/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal-key/"
       ];
     };
-  
+    
+    # Screenshot
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/screenshot-key" = {
       name = "Take Screenshot with Flameshot";
       command = "script --command 'flameshot gui'";
-      binding = "<Super>Home";  # Ctrl + Home
+      binding = "<Super>Home";
+    };
+    
+    # File Explorer
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/file-explorer-key" = {
+      name = "Open File Explorer";
+      command = "nautilus";
+      binding = "<Super>E";
+    };
+    
+    # System Monitor
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/system-monitor-key" = {
+      name = "Open System Monitor";
+      command = "gnome-system-monitor";
+      binding = "<Primary><Shift>Escape";
+    };
+    
+    # Terminal
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal-key" = {
+      name = "Open Terminal";
+      command = "warp-terminal";
+      binding = "<Super>T";
     };
   };
   
