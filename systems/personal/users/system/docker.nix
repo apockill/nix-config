d7f898d7238f6cad69ec3ae68501cab476e9fd9b
Alex Thiele <apocthiel@gmail.com>
2025-01-14 14:56:16 -0800

    Clean up nvidia config

--- systems/personal/users/system/docker.nix
+++ systems/personal/users/system/docker.nix
@@ -1,9 +1,7 @@
 {...}: {
+  # The users are added to the "docker" group in their users/USER/default.nix
   virtualisation.docker = {
     enable = true;
     enableNvidia = true;
   };
-
-  # The users are added to the "docker" group in their users/USER/default.nix
-
 }
