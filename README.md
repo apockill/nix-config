ade9f6da84e6e53c58e676e43fbf17feb5c17c94
Alex Thiele <apocthiel@gmail.com>
2025-01-14 10:26:05 -0800

    Add firmware usage and instructions

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Everything in this set up is likely not "Strictly Correct (TM)", since it's my f
 ## Steps
 
 Clone the repository and symlink nix configuration
-```
+```shell
 git clone git@github.com:apockill/dotfiles.git
 cd dotfiles
 sudo nixos-rebuild --flake . switch
@@ -13,6 +13,12 @@ sudo nixos-rebuild --flake . switch
 
 If you run into an error about "blah blah does not provide attribute..." it's possible you're chaning the hostname from what existed prior. If so, you may need to run the following once (assumes agilite is your hostname):
 
-```
+```shell
 sudo nixos-rebuild --flake .#agilite switch
+```
+
+Next, it's probably a good idea to make sure your firmware is up to date. You can do
+this via `gnome-firmware`, or CLI:
+```shell
+sudo fwupdmgr update
 ```
--- systems/personal/default.nix
+++ systems/personal/default.nix
@@ -14,35 +14,18 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "agilite"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Enable flakes
+  # Some general system settings
+  networking.hostName = "agilite"; # Define your hostname.
+  time.timeZone = "America/Los_Angeles";
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ALL = "en_US.UTF-8";
+  };
   nix.settings = {
     experimental-features = ["nix-command" "flakes"];
     auto-optimise-store = true;
-  };
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Set your time zone.
-  time.timeZone = "America/Los_Angeles";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
   };
 
   # Configure keymap in X11
@@ -55,6 +38,9 @@
   networking.networkmanager.enable = true;
   services.printing.enable = true;
   nixpkgs.config.allowUnfree = true;
+
+  # Firmware update manager, usable via gnome-firmware
+  services.fwupd.enable = true;
 
   # WM Setup
   services.xserver.enable = true;
@@ -69,12 +55,6 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
@@ -102,6 +82,12 @@
     dconf-editor
     gnome-tweaks
     anydesk
+    vlc
+    gimp
+    kdenlive
+
+    # CLI Applications
+    tree
     
     # Screenshots
     flameshot
@@ -117,8 +103,10 @@
 
     # Alternative package managers
     pipx
-    
-    
+
+    # Firmware updates
+    pkgs.fwupd
+    gnome-firmware
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
