aebe0cffbae66d05fcb103064366e4d63df565f9
Alex Thiele <apocthiel@gmail.com>
2025-01-21 09:34:45 -0800

    Alias `edit` for gnome-text-editor

--- systems/personal/users/system/bash.nix
+++ systems/personal/users/system/bash.nix
@@ -2,6 +2,7 @@
   # Set up default bash aliases
   programs.bash.shellAliases = {
     docker-killall = "docker container stop $(docker container ls -aq) && docker container rm $(docker container ls -aq)";
+    edit = "gnome-text-editor";
   };
 
 }
