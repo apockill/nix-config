12b9f5c8626baf1718998cff5608031c58709e21
Alex Thiele <apocthiel@gmail.com>
2025-02-28 16:12:50 -0800

    Fix docker-killall alias

--- systems/personal/users/system/bash.nix
+++ systems/personal/users/system/bash.nix
@@ -4,9 +4,11 @@
     # Kill and delete all containers that don't have the name 'box' in the name.
     # This prevents distrobox or toolbox containers from being accidentally deleted.
     docker-killall = ''
+      echo "Stopping non-distrobox containers..." && \
+      docker ps -a --format '{{.ID}} {{.Image}}' | grep -v 'box' | awk '{print $1}' | xargs -r docker container stop && \
       echo "Removing non-distrobox containers..." && \
-      docker container stop $(docker ps -aq --filter "ancestor=$(docker images --format "{{.Repository}}" | grep -v 'box')") && \
-      docker container rm $(docker ps -aq --filter "ancestor=$(docker images --format "{{.Repository}}" | grep -v 'box')")
+      docker ps -a --format '{{.ID}} {{.Image}}' | grep -v 'box' | awk '{print $1}' | xargs -r docker container rm && \
+      echo "Done!"
     '';
     edit = "gnome-text-editor";
     dbe = "distrobox enter";
