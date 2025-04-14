0db365ef823a1efe9f0859ee0b10bd700baca0a7
Alex Thiele <apocthiel@gmail.com>
2025-04-14 08:54:43 -0700

    Forgot to add 'blur my shell' to gnome extensions turned on

--- systems/personal/users/alex/gnome_extensions.nix
+++ systems/personal/users/alex/gnome_extensions.nix
@@ -9,8 +9,12 @@
     # Enable gnome extensions
     "org/gnome/shell" = {
       enabled-extensions = [
+        # User installed
         pkgs.gnomeExtensions.pop-shell.extensionUuid
         pkgs.gnomeExtensions.another-window-session-manager.extensionUuid
+        pkgs.gnomeExtensions.blur-my-shell.extensionUuid
+
+        # Came with system
         pkgs.gnomeExtensions.system-monitor.extensionUuid
         pkgs.gnomeExtensions.workspace-indicator.extensionUuid
         pkgs.gnomeExtensions.auto-move-windows.extensionUuid
