0a251291ad2851854bb652c5bdafc467c4d86008
Alex Thiele <apocthiel@gmail.com>
2025-04-17 08:41:17 -0700

    Formatting

--- systems/personal/users/alex/gnome_extensions.nix
+++ systems/personal/users/alex/gnome_extensions.nix
@@ -30,21 +30,24 @@
       window-border-color = "rgb(26,95,180)";
 
       # Key bindings
-      tiling-system-activation-key = [ "2" ];  # Super
-      span-multiple-tiles-activation-key = [ "1" ];  # alt
-      tiling-system-deactivation-key = ["-1"];  # Disable deactivation
+      tiling-system-activation-key = [ "2" ]; # Super
+      span-multiple-tiles-activation-key = [ "1" ]; # alt
+      tiling-system-deactivation-key = [ "-1" ]; # Disable deactivation
 
       # Other
       enable-autotiling = true;
       enable-tiling-system-windows-suggestions = true;
       enable-screen-edges-windows-suggestions = true;
-      restore-window-original-size = false;  # When untiled, don't restore original size
+      restore-window-original-size =
+        false; # When untiled, don't restore original size
 
       # Map workspaces to layouts
-      selected-layouts = [ ["Layout 3"] ["Layout 4"] ["Layout 2"] ["Layout 2"] ];
+      selected-layouts =
+        [ [ "Layout 3" ] [ "Layout 4" ] [ "Layout 2" ] [ "Layout 2" ] ];
 
       # JSON dump for the layouts I use
-      layouts-json = ''[{"id":"Layout 1","tiles":[{"x":0,"y":0,"width":0.22,"height":0.5,"groups":[1,2]},{"x":0,"y":0.5,"width":0.22,"height":0.5,"groups":[1,2]},{"x":0.22,"y":0,"width":0.56,"height":1,"groups":[2,3]},{"x":0.78,"y":0,"width":0.22,"height":0.5,"groups":[3,4]},{"x":0.78,"y":0.5,"width":0.22,"height":0.5,"groups":[3,4]}]},{"id":"Layout 2","tiles":[{"x":0,"y":0,"width":0.286328125,"height":1,"groups":[1]},{"x":0.286328125,"y":0,"width":0.42734374999999997,"height":1,"groups":[2,1]},{"x":0.713671875,"y":0,"width":0.28632812500000004,"height":1,"groups":[2]}]},{"id":"Layout 3","tiles":[{"x":0,"y":0,"width":0.5,"height":1,"groups":[1]},{"x":0.5,"y":0,"width":0.49999999999999983,"height":1,"groups":[1]}]},{"id":"Layout 4","tiles":[{"x":0,"y":0,"width":0.2853515625,"height":1,"groups":[2]},{"x":0.2853515625,"y":0,"width":0.42832031249999997,"height":1,"groups":[3,2]},{"x":0.713671875,"y":0,"width":0.2863281250000001,"height":0.4928977272727273,"groups":[4,3]},{"x":0.713671875,"y":0.4928977272727273,"width":0.2863281250000001,"height":0.5071022727272725,"groups":[4,3]}]}]'';
+      layouts-json = ''
+        [{"id":"Layout 1","tiles":[{"x":0,"y":0,"width":0.22,"height":0.5,"groups":[1,2]},{"x":0,"y":0.5,"width":0.22,"height":0.5,"groups":[1,2]},{"x":0.22,"y":0,"width":0.56,"height":1,"groups":[2,3]},{"x":0.78,"y":0,"width":0.22,"height":0.5,"groups":[3,4]},{"x":0.78,"y":0.5,"width":0.22,"height":0.5,"groups":[3,4]}]},{"id":"Layout 2","tiles":[{"x":0,"y":0,"width":0.286328125,"height":1,"groups":[1]},{"x":0.286328125,"y":0,"width":0.42734374999999997,"height":1,"groups":[2,1]},{"x":0.713671875,"y":0,"width":0.28632812500000004,"height":1,"groups":[2]}]},{"id":"Layout 3","tiles":[{"x":0,"y":0,"width":0.5,"height":1,"groups":[1]},{"x":0.5,"y":0,"width":0.49999999999999983,"height":1,"groups":[1]}]},{"id":"Layout 4","tiles":[{"x":0,"y":0,"width":0.2853515625,"height":1,"groups":[2]},{"x":0.2853515625,"y":0,"width":0.42832031249999997,"height":1,"groups":[3,2]},{"x":0.713671875,"y":0,"width":0.2863281250000001,"height":0.4928977272727273,"groups":[4,3]},{"x":0.713671875,"y":0.4928977272727273,"width":0.2863281250000001,"height":0.5071022727272725,"groups":[4,3]}]}]'';
     };
 
     # Configure "Auto Move Windows" extension with specific application -> workspace pairs
