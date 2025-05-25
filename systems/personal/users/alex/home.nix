234d27fbe65bf7e10f4b4b2afeccafbafb5796cd
Alex Thiele <apocthiel@gmail.com>
2025-05-25 15:55:43 -0700

    Add chrome

--- systems/personal/users/alex/home.nix
+++ systems/personal/users/alex/home.nix
@@ -1,8 +1,9 @@
 { config, pkgs, lib, ... }: {
-  imports = [ ./dconf.nix ./git.nix ./gnome_extensions.nix ./firefox.nix ];
+  imports = [ ./dconf.nix ./git.nix ./gnome_extensions.nix ./firefox.nix ./chrome.nix];
 
   # Enable home-manager
   programs.home-manager.enable = true;
+  nixpkgs.config.allowUnfree = true;
 
   # Set home-manager state version
   home.stateVersion = "24.11";
