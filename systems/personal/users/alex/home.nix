f9538f41f87aee9bcf7d5f4ef7b8b141b920ae02
Alex Thiele <apocthiel@gmail.com>
2025-06-22 17:05:29 -0700

    Update & fix virtualbox

--- systems/personal/users/alex/home.nix
+++ systems/personal/users/alex/home.nix
@@ -1,5 +1,6 @@
 { config, pkgs, lib, ... }: {
-  imports = [ ./dconf.nix ./git.nix ./gnome_extensions.nix ./firefox.nix ./chrome.nix];
+  imports =
+    [ ./dconf.nix ./git.nix ./gnome_extensions.nix ./firefox.nix ./chrome.nix ];
 
   # Enable home-manager
   programs.home-manager.enable = true;
