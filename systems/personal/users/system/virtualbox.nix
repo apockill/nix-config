c19bc12f46b0e0239bbcb272c32fbfca6ce33473
Alex Thiele <apocthiel@gmail.com>
2025-08-02 09:00:36 -0700

    Update & fix virtualbox

--- systems/personal/users/system/virtualbox.nix
+++ systems/personal/users/system/virtualbox.nix
@@ -1,3 +1,8 @@
 { inputs, pkgs, ... }: {
-  virtualisation.virtualbox.host.enable = true;
+  users.extraGroups.vboxusers.members = [ "alex" ];
+  virtualisation = {
+    virtualbox.host.enable = true;
+    # If USB is not needed, comment this out. It may cause frequent recompilations.
+    virtualbox.host.enableExtensionPack = true;
+  };
 }
